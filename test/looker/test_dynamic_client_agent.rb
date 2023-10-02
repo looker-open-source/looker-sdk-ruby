caba36383ee9bdc4d6721af5e3b1e5ebc2242023
ydah <13041216+ydah@users.noreply.github.com>
2023-10-02 11:34:35 +0900

    Use params instead of parms

--- test/looker/test_dynamic_client_agent.rb
+++ test/looker/test_dynamic_client_agent.rb
@@ -71,7 +71,7 @@ describe LookerSDK::Client::Dynamic do
       end
     end
 
-    it "get with parms" do
+    it "get with params" do
       mock = MiniTest::Mock.new.expect(:call, response, [:get, '/api/3.0/users/25', nil, {}])
       with_stub(Sawyer::Agent, :new, mock) do
         sdk.user(25)
